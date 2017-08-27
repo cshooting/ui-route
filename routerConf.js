20062f73f74109250797ba6c8f80f693c9fa728e
cshooting <key874@qq.com>
2017-08-27 23:08:11 +0800

    shooting

--- routerConf.js
+++ routerConf.js
@@ -1,5 +1,4 @@
 var myApp = angular.module("myApp", ["ui.router"]);
-//这里叫做App模块，这将告诉HTML页面这是一个AngularJS作用的页面，并把ui-router注入AngularJS主模块，它的内容由AngularJS引擎来解释。
 myApp.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
     $stateProvider
